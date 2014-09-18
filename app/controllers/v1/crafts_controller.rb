bd7fe9ffbf0c6843d2cdee49077e15e9b0591adb
minh <mreigen@gmail.com>
2014-09-17 22:38:58 -0700

    add action: index, show and create

--- app/controllers/v1/crafts_controller.rb
+++ app/controllers/v1/crafts_controller.rb
@@ -3,12 +3,26 @@ class V1::CraftsController < ApplicationController
   respond_to :json
 
   def index
-    render json: {yo: :empty}
+    render_success(result: Craft.all)
   end
 
   def show
-    guid = params[:guid]
-    # render_error("missing craft's id", :bad_request) unless guid
-    render_success({result: Craft.find_by_guid(guid)})
+    guid = params[:id]
+    if guid.blank?
+      render_error("missing craft's id", :bad_request)
+    else
+      render_success({result: Craft.find_by_guid(guid)})
+    end
   end
+
+  def create
+    type  = params[:type]
+    if type.blank?
+      render_error("missing craft's type", :bad_request)
+    else
+      craft = Craft.create(type: type)
+      render_success(result: craft)
+    end
+  end
+
 end
