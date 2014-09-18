8edbbf7bcc07e87c34c5526c208a04f864d02c66
minh <mreigen@gmail.com>
2014-09-17 22:58:36 -0700

    action destroy

--- app/controllers/v1/crafts_controller.rb
+++ app/controllers/v1/crafts_controller.rb
@@ -11,17 +11,32 @@ class V1::CraftsController < ApplicationController
     if guid.blank?
       render_error("missing craft's id", :bad_request)
     else
-      render_success({result: Craft.find_by_guid(guid)})
+      render_success(result: Craft.find_by_guid(guid))
     end
   end
 
   def create
-    type  = params[:type]
+    type = params[:type]
     if type.blank?
       render_error("missing craft's type", :bad_request)
     else
       craft = Craft.create(type: type)
       render_success(result: craft)
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    if id.blank?
+      render_error("missing craft's id", :bad_request)
+    else
+      craft = Craft.find_by_guid(id)
+      if craft.blank?
+        render_error("Can't find craft with id: #{id}", :bad_request)
+      else
+        craft.delete
+        render_success(result: "craft with id: #{id} has been deleted")
+      end
     end
   end
 
