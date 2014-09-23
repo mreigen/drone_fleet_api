4a3e4df3e0b771a2aa7681a27126ac564038b94e
minh <mreigen@gmail.com>
2014-09-22 22:14:38 -0700

    beautify!

--- app/models/concerns/paramable.rb
+++ app/models/concerns/paramable.rb
@@ -1,10 +1,6 @@
 module Paramable
-  # Prune the msg_hsh[:params] of any attributes
-  # the model doesn't respond to
-  # and returns the keys
+  # returns only the attributes that the current class allows
   def allowed_keys_in_params(params_hsh)
-    params_hsh.collect do |k,_|
-      return k.to_sym if self.attribute_names.include? k.to_s
-    end
+    self.attribute_names & params_hsh.keys
   end
 end
