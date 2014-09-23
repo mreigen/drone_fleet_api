7996da4f3d91806197ba053bde25c201d6acdf63
minh <mreigen@gmail.com>
2014-09-23 01:27:52 -0700

    run swagger docs after deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,10 @@ namespace :deploy do
   end
 
   after :finishing, 'deploy:cleanup'
+
+  task :cleanup do
+    execute :rake, "swagger:docs"
+  end
 end
 
 set :linked_files, %w{config/mongoid.yml}
@@ -61,25 +65,3 @@ end
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
